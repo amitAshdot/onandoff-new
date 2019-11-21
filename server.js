cbaa0527f85fa9afd5afe477e49b20c356f9e519
Amit Ashdot <amitashdot@gmail.com>
2019-11-21 17:09:07 +0200

    server.js changed

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use('/api/file', require('./routes/file'));
 
 app.use('/api/websites', require('./routes/websites'));
 
-// //Server static assets in production
-// if(procces.env.NODE_ENV === 'production'){
-//     //Set static folder
-//     app.use(express.static('client/build'));
-//     app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
-// }
+//Server static assets in production
+if(procces.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5002;
 
