12403ef64744b8c7de516941460db85601435222
Amit Ashdot <amitashdot@gmail.com>
2019-12-17 14:01:31 +0200

    bug fixed

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use('/api/file', require('./routes/file'));
 app.use('/api/timersplus', require('./routes/timersplus'));
 app.use('/api/websites', require('./routes/websites'));
 
-app.use(cors());
 
 //Server static assets in production
 if(process.env.NODE_ENV === 'production'){
+    app.use(cors());
+
     //Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
